3babd8b8617202e9c5228045101deaf0f855101c
Caleb Kats <calebkats0@gmail.com>
2022-05-25 09:23:29 -0500

    My Commit

---
+++ src/LottoPicker.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class LottoPicker {
+    public static void main(String[] args){
+        // dataType variableName = theValueToBeStored
+        String nameOfIt = "Application started...";
+        System.out.println(nameOfIt);
+        Random random = new Random();
+
+
+        int numberOfNumbersGenerated = 0;
+        int total = 5;
+
+        while(numberOfNumbersGenerated < total){
+            int randomNumber = random.nextInt(30);
+            System.out.println(randomNumber);
+            numberOfNumbersGenerated = numberOfNumbersGenerated + 1;
+        }
+    }
+}
